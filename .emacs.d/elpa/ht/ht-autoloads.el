53b3cb08df496625b35bb5b8f436030677077ea8
TsubasaWp <tsubasa.wp@gmail.com>
2016-01-22 22:16:55 +0800

    add elpa folder

---
+++ .emacs.d/elpa/ht/ht-autoloads.el
@@ -0,0 +1,15 @@
+;;; ht-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ht.el") (22170 11714 114960 300000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ht-autoloads.el ends here
